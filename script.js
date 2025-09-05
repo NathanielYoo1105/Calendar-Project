e62f64052e20ebdbab29c0bf408d3b54a9910da5
NathanielYoo1105 <142622848+NathanielYoo1105@users.noreply.github.com>
2025-09-05 11:06:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const myButton = document.getElementById('myButton');
+const messageParagraph = document.getElementById('message');
+myButton.addEventListener('click', function() {
+    messageParagraph.textContent = 'Button was clicked!';
+});
+myButton.addEventListener('click', function() {
+    myButton.style.backgroundColor = 'blue';
+    myButton.style.color = 'white';
+});
